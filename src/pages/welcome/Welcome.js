ec6465f53400c96097eb9d54cec79089e86547f7
Markus Hay <markus.hay@gmail.com>
2017-08-04 21:06:06 -0500

    ADDED globals for easy configuration of colors and typography

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -8,8 +8,14 @@ const Welcome = () => (
     <LinearProgress mode="indeterminate" />
     
     <div className="content" id="welcome">
+      <aside>
+        <h3>Aside</h3>
+
+
+      </aside>
+
       <main>
-        <h3>Welcome</h3>
+        <h3>Main</h3>
 
         <Message
           isControl
@@ -41,7 +47,20 @@ const Welcome = () => (
       </main>
 
       <aside>
-        aside
+        <h3>Aside</h3>
+
+        <p>
+          Spicy jalapeno bacon ipsum dolor amet pig meatball meatloaf shankle, ground round venison sausage 
+          tri-tip shank short ribs jowl flank andouille tongue. Frankfurter kielbasa shankle, venison shoulder 
+          pig jowl pork chop. Bresaola brisket cupim chicken shankle meatloaf cow sausage porchetta. Spare ribs 
+          pancetta tenderloin, ribeye pork loin jerky landjaeger andouille pork chop.
+        </p>
+
+        <p>
+          Strip steak pancetta flank tail boudin sausage ham hock andouille. Chuck corned beef fatback ham hock meatball. 
+          Hamburger boudin fatback ham. Drumstick tail strip steak, doner venison shoulder fatback tongue short loin pig. 
+          Hamburger short ribs sirloin, cupim meatball kielbasa ground round bacon fatback.
+        </p>
       </aside>
     </div>
   </div>
